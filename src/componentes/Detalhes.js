7d4e5124af396cce80d58479fd883553070ab907
marinamedeirosz <marinapaffer@gmail.com>
2022-09-04 14:28:59 -0300

    ..

--- src/componentes/Detalhes.js
+++ src/componentes/Detalhes.js
@@ -1,14 +1,17 @@
 import { StyleSheet, View, Image, Dimensions } from "react-native";
 import Texto from "./Texto";
 
+let cor;
 
 export default function Detalhes(props) {
+    cor=props.cor
+
     return (
         <View style={styles.detalhesView}>
-            <Image style={styles.detalhesImg} source={require("../../assets/vento.png")} />
-            <View id="text"style={styles.detalhesView2}>    
-                <Texto style={styles.detalhesText} texto="WIND" cor="#7EB1FF"></Texto>
-                <Texto style={styles.detalhesText} texto="19.2 km/j" cor="#FEFFFF"></Texto>
+            <Image style={styles.detalhesImg} source={require(props.img)} />
+            <View style={styles.detalhesView2}>    
+                <Texto style={styles.detalhesText} texto={props.texto} cor="#7EB1FF"></Texto>
+                <Texto style={styles.detalhesText} texto={props.texto2} cor="#FEFFFF"></Texto>
             </View>
         </View>
   );
@@ -17,7 +20,7 @@ export default function Detalhes(props) {
 const styles = StyleSheet.create({
     detalhesView: {
         flexDirection: "row",
-        margin: 4
+        alignItems: "center"
     },
     detalhesView2:{
         margin: 2,
--- src/telas/Clima.js
+++ src/telas/Clima.js
@@ -2,20 +2,26 @@ import { StyleSheet, View, Image } from "react-native";
 import Texto from "../componentes/Texto";
 import Detalhes from "../componentes/Detalhes";
 import Temperatura from "../componentes/Temperatura";
+import img1 from "../../assets/vento.png"
 
 
-export default function Local(props) {
+export default function Clima() {
   return (
     <View style={styles.climaView}>
-        <Image style={styles.climaImg} source={require("../../assets/dia-nublado.png")} />
+        <Image style={styles.climaImg} source={require(img1)} />
         <Texto texto="Heavy Rain" cor="#F8FFFF" fonte="22"></Texto>
         <Texto texto="Sunday, 02 Oct" cor="#7ea1e6" fonte="18"></Texto>
         <Temperatura t="24°"></Temperatura>
-        <Detalhes></Detalhes> 
+        <View style={styles.climaView2}>
+          <Detalhes texto="WIND" texto2="19.2 km/j" img="../../assets/vento.png"></Detalhes>
+          <Detalhes texto="FEELS LIKE" texto2="25°"></Detalhes>
+          <Detalhes texto="INDEX UV" texto2="2"></Detalhes>
+          <Detalhes texto="PRESSURE" texto2="1014 mbar"></Detalhes>
+        </View> 
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
     climaView: {
         backgroundColor: "#437AFF",
@@ -23,6 +29,13 @@ const styles = StyleSheet.create({
         margin: 10, 
         alignItems: "center"
     }, 
+    climaView2: {
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      flexWrap: "wrap",
+      alignItems: "flex-start",
+      margin: 5
+    },
     climaImg: {
         width: 50,
         height: 50,
